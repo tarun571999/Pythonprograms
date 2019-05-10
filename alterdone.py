a3385b61b91890e5e307238d6b98698338ac6aa8
Tharun Kumar T <44298833+tarun571999@users.noreply.github.com>
2019-05-10 12:10:22 +0530

    Update alterdone.py

--- alterdone.py
+++ alterdone.py
@@ -1,3 +1,5 @@
+#problem statement : task is to accept input from the user untill user enters Done, if user enters done print sum,count ,maximum,minimum,and average of all the number the user has entered
+
 l=[]
 while(1):
 	input1 = input('enter')
